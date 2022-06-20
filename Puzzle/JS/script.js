61d56daf49560ddb710c4370d9131d4e8f4ed179
Luis/ <82966665+DEVLuisz@users.noreply.github.com>
2022-06-20 12:08:13 -0300

    Add files via upload

---
+++ Puzzle/JS/script.js
@@ -0,0 +1,95 @@
+var rows = 7;
+var columns = 7;
+
+var currTile;
+var otherTile;
+var timeStartet = 200;
+
+var turns = 0;
+
+
+window.onload = function() {
+   
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            
+            let tile = document.createElement("img");
+            tile.src = "./Img/Blank.jpg";
+
+            //DRAG FUNCTIONALITY
+            tile.addEventListener("dragstart", dragStart); 
+            tile.addEventListener("dragover", dragOver);   
+            tile.addEventListener("dragenter", dragEnter); 
+            tile.addEventListener("dragleave", dragLeave); 
+            tile.addEventListener("drop", dragDrop);      
+            tile.addEventListener("dragend", dragEnd);     
+
+            document.getElementById("board").append(tile);
+        }
+    }
+
+
+    let pieces = [];
+    for (let i=1; i <= rows*columns; i++) {
+        pieces.push(i.toString()); 
+    }
+    pieces.reverse();
+    for (let i =0; i < pieces.length; i++) {
+        let j = Math.floor(Math.random() * pieces.length);
+
+        
+        let tmp = pieces[i];
+        pieces[i] = pieces[j];
+        pieces[j] = tmp;
+    }
+
+    for (let i = 0; i < pieces.length; i++) {
+        let tile = document.createElement("img");
+        tile.src = "./Img/" + pieces[i] + ".jpg";
+
+
+        tile.addEventListener("dragstart", dragStart);
+        tile.addEventListener("dragover", dragOver);   
+        tile.addEventListener("dragenter", dragEnter); 
+        tile.addEventListener("dragleave", dragLeave); 
+        tile.addEventListener("drop", dragDrop);       
+        tile.addEventListener("dragend", dragEnd);      
+
+        document.getElementById("pieces").append(tile);
+    }
+}
+
+
+function dragStart() {
+    currTile = this; 
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this; 
+}
+
+function dragEnd() {
+    if (currTile.src.includes("Blank")) {
+        return;
+    }
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+    currTile.src = otherImg;
+    otherTile.src = currImg;
+
+    turns += 1;
+    document.getElementById("turns").innerText = turns;
+
+}
